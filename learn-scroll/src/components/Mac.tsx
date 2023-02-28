68be17fc70ac9c20e5650953db39629f13471c37
Thabish <kadertabish2@gmail.com>
2023-02-28 11:50:47 +0400

    orientation-fixed
    Signed-off-by: Thabish <kadertabish2@gmail.com>

--- learn-scroll/src/components/Mac.tsx
+++ learn-scroll/src/components/Mac.tsx
@@ -6,19 +6,19 @@ export const Mac = () => {
 	const { nodes, materials } = useGLTF("/mac.glb") as MacGLTF;
 	const { rotation } = useControls({
 		rotation: {
-			value: { x: 0, y: 0, z: 0 },
+			value: { x: -1.56, y: 3.09, z: 1.45 },
 		},
 	});
 	return (
 		<>
-			<group rotation={[0, Math.PI * 0.5, 0]}>
+			<group position={[0, -0.43, -11.35]} rotation={[Math.PI / 2, 0, 0]}>
 				<mesh geometry={nodes.back_1.geometry} />
 				<mesh geometry={nodes.back_2.geometry} />
 			</group>
-			{/* <mesh geometry={nodes.body_1.geometry} />
-			<mesh geometry={nodes.body_2.geometry} />
+			<mesh geometry={nodes.body_1.geometry} />
+			{/* <mesh geometry={nodes.body_2.geometry} />
 			<mesh geometry={nodes.matte.geometry} />
-			<mesh geometry={nodes.screen.geometry} /> */}
+			<mesh geometry={nodes.screen.geometry} />  */}
 		</>
 	);
 };
