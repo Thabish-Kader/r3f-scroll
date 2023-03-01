defe405c26566ea0a6988ce2065d7be42b556dd6
Thabish <kadertabish2@gmail.com>
2023-03-01 12:58:47 +0400

    inital-setup
    Signed-off-by: Thabish <kadertabish2@gmail.com>

---
+++ shoes/src/components/Jordan.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+// Source : https://sketchfab.com/3d-models/air-jordan-1-a4b434181fbb48008ad460722fd53725
+export const Jordan = () => {
+	return <div>Jordan</div>;
+};
